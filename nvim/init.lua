1534f6f09c8cd1464faa1bd1aeb4a5cdce0be7ca
Murphy Lee <murphylee2004@gmail.com>
2024-02-20 00:14:35 -0500

    config for LuaSnip

--- nvim/init.lua
+++ nvim/init.lua
@@ -30,12 +30,19 @@ local plugins = {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "go", "javascript", "html" }, 
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "javascript", "html" }, 
         sync_install = false,
         highlight = { enable = true },
         indent = { enable = true }
       })
     end
+  },
+  {
+	  "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.2.0", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp"
   }
 }
 local opts = {}
@@ -46,3 +53,13 @@ vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 
 vim.cmd.colorscheme "catppuccin"
+vim.cmd[[
+" Use Tab to expand and jump through snippets
+imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
+smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+
+" Use Shift-Tab to jump backwards through snippets
+imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+]]
+require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/LuaSnip/"})
